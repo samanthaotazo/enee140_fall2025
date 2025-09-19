c4eac480297eba6c1c83ffa2729f94ceb0a06da7
samanthaotazo <164980722+samanthaotazo@users.noreply.github.com>
2025-09-19 09:32:01 -0400

    Create sept17practice.c

---
+++ sept17practice.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/*Use getchar to take in some input from the CLion terminal. Make sure to keep getting characters until EOF is reached.
+* As you go, print any uppercase letter back to the output. Use a while loop. */
+int main() {
+   int c;
+  
+   //while loop, to keep taking in characters until EOF is reached
+   while ((c = getchar()) != EOF){
+       //Check if uppercase
+       if (c >= 'A' && c <= 'Z'){ //How can I do this with isupper?
+           //I can print using putchar or printf... how do I do it both ways?
+           //Method 1
+           //putchar(c);
+           //Method 2
+           printf("%c", c);
+       }
+   }
+   return 0;
+}
+
+/* If you finish early... change your while loop to a for loop */
+
