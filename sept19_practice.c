0b8a975a4468c0cdcdef6d5b5010c1faa48c4d31
samanthaotazo <164980722+samanthaotazo@users.noreply.github.com>
2025-09-19 09:37:38 -0400

    Create sept19_practice.c

---
+++ sept19_practice.c
@@ -0,0 +1,14 @@
+/*Created 19 September 2025 by Samantha Otazo*/
+#include <stdio.h>
+
+int main(void) {
+    //Question 1
+    /*Sum the first 5 terms of the Fibonacci sequence using
+     * a for loop, and store your answer in a variable
+     * of integer type that you call answer. */
+    //Note: It goes (0, 1, 1, 2, 3, 5, 8, 13, 21, 34)
+
+    //Question 2
+    /* Now, convert your answer to a while loop.*/
+    return 0;
+}
