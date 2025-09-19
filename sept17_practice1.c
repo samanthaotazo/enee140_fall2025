7ead32342e70c377d4fb52e6d0f943f556fe5c6e
samanthaotazo <164980722+samanthaotazo@users.noreply.github.com>
2025-09-19 14:06:34 -0400

    Create sept17_practice1.c

---
+++ sept17_practice1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void) {
+    //Question 1
+    /*Write a for loop that prints a statement
+     * ("Hello World") four times. */
+    /*
+    int i;
+    for (i = 0;i < 4; i++){
+        //Do something here...in our case we want to print
+        printf("Hello World\n");
+    }*/
+
+    //Question 2
+    /*Write a for loop that sums the numbers 1 through 5 (ie.
+     * adds 1 + 2 + 3 + 4 + 5. Print the answer outside of the loop. */
+    //We declare some variables
+    /*int sum = 0; //sum will store our result
+    int a; // a will track our position in the loop/ through 1 to 5
+
+    for (a = 1;a <= 5; a++){
+        sum = sum + a;
+    }
+    printf("Result is %d", sum);
+    */
+
+    //Question 3
+    /* Convert question 1 to a while loop */
+    int i = 0;
+    while(i <= 3){
+        printf("Hello World\n");
+        i++;
+    }
+    return 0;
+}
