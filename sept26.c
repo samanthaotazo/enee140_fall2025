cd247a58d9fb21ec4d86e57b40d307474789f6eb
samanthaotazo <164980722+samanthaotazo@users.noreply.github.com>
2025-09-26 13:37:00 -0400

    Create sept26.c

---
+++ sept26.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+
+int add2 (int input);
+int add2andprint(int input);
+float divideby2(float input);
+int getnumber(void);
+
+
+int main(void) {
+    int mynumber = 10;
+    float myfloat = 5.0;
+    int result;
+    float floatresult;
+    //Part 1: Practice with functions
+    //Question 1:
+    /*Write a function that adds 2 to an integer
+     * input and returns the value as an integer. Print the value. */
+    //Option 1: print the value without storing it anywhere. Now try storing it
+    // then printing it
+    //printf("Result is %d", add2(mynumber));
+    //Option 2:
+    //Store the result in a variable
+    /*result = add2(mynumber);
+    printf("Result is %d", result);*/
+
+    //Question 2:
+    /* Write a function that takes in an integer, adds 2 to the number,
+     * prints the value, then returns the value 1 as an integer. */
+    //result = add2andprint(mynumber);
+    //printf("Result is %d", result);
+    //Question 3
+    /* Write a function that takes in a float, divides it by 2,
+     * then returns the result as a float. Print the result */
+    //floatresult = divideby2(myfloat);
+    //printf("result is %.2f", floatresult);
+
+
+
+
+    //Part 2: Practice with scanf
+    //Question 1
+    //Prompt the user to enter a number between 1 and 10, using a printf statement
+    //Scan in the number
+    //Print "Your number is _."
+    /*int userinput;
+    printf("\nEnter a number between 1 and 10: ");
+    scanf("%d", &userinput);
+    printf("\nYour number is %d", userinput);*/
+
+
+    //Question 2
+    //Prompt the user to enter the first letter of their name
+    //Scan it in as a character
+    //Print the statement: "Nice to meet you, _"
+    char firstletter;
+    printf("\nWhat is the first letter of your name: ");
+    scanf("%c", &firstletter);
+    printf("Nice to meet you, %c", firstletter);
+
+    //Question 3
+    /* Now, do Part B questions 1 and 2 with functions. Return the specified value and
+     * print it inside main. */
+    int result1;
+    result1 = getnumber();
+    printf("Your number is %d", result1);
+
+    return 0;
+}
+int add2 (int input){
+    int number = input + 2;
+    return number;
+}
+int add2andprint(int input){
+    printf("Inside the function we get %d\n", input + 2);
+    return 1;
+}
+float divideby2(float input){
+    return input/2.0;
+}
+int getnumber(void){
+    int userinput;
+    
+    //Prompt for user input
+    printf("\nEnter a number between 1 and 10: ");
+    
+    //scan it into the variable you made
+    scanf("%d", &userinput);
+    //return the value, you will print it in main
+    return userinput;
+}
